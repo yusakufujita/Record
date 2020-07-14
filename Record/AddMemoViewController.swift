15de7fb6bad81ec1a6a6dfa8ca2913efbdd2c713
藤田優作 <hinasakun@outlook.jp>
2020-07-15 06:29:12 +0900

    ログイン機能と新規登録機能完成

--- Record/AddMemoViewController.swift
+++ Record/AddMemoViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class AddMemoViewController:
 UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate, UITextViewDelegate,UIScrollViewDelegate {
     
@@ -204,17 +205,14 @@ UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate,
            alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel, handler: nil))
            self.present(alert, animated: true, completion:nil)
        }
-    
 
     /*
     // MARK: - Navigation
-
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
     */
-
 }
 
--- Record/SignUpViewController.swift
+++ Record/SignUpViewController.swift
@@ -11,7 +11,14 @@ import TextFieldEffects
 import Firebase
 
 class SignUpViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var email: KaedeTextField!
+    
+    @IBOutlet weak var password: KaedeTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,6 +39,43 @@ class SignUpViewController: UIViewController {
     }
     */
 
+    @IBAction func DoAction(_ sender: Any) {
+        let email = self.email.text
+        
+        let password = self.password.text
+        
+          Auth.auth().createUser(withEmail: email ?? "", password: password ?? "") { [weak self] result, error in
+             guard let self = self else { return }
+             if let user = result?.user {
+                 let req = user.createProfileChangeRequest()
+                // req.displayName = name
+                 req.commitChanges() { [weak self] error in
+                     guard let self = self else { return }
+                     if error == nil {
+                         user.sendEmailVerification() { [weak self] error in
+                             guard let self = self else { return }
+                             if error == nil {
+                                 // 仮登録完了画面へ遷移する処理
+                             }
+                             self.showErrorIfNeeded(error)
+                         }
+                     }
+                     self.showErrorIfNeeded(error)
+                 }
+             }
+             self.showErrorIfNeeded(error)
+         }
+    }
+    
+    private func showErrorIfNeeded(_ errorOrNil: Error?) {
+        // エラーがなければ何もしません
+        guard let error = errorOrNil else { return }
+        
+        let message = "エラーが起きました" // ここは後述しますが、とりあえず固定文字列
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
     
     
 }
